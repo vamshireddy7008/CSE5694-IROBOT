b7d3dabb0c8fdc6a374c3655507b45a39abb1cdb
skyfall1818 <phid8181@hotmail.com>
2024-10-27 02:21:09 -0400

    Update pages tp include indents
    Update pages tp include indents. include algorithm explanation.

--- irobot_door_sensor/BN_Test.py
+++ irobot_door_sensor/BN_Test.py
@@ -2,23 +2,24 @@ from irobot_BN import IrobotNetwork as IRN
 
 def main():
     network = IRN()
+    print(network.calculate_probability())
     network.add_scanner_value(149)
     network.add_wheel_value(2.4)
     network.add_bumper_value(0)
 
-    network.add_scanner_value(84)
+    network.add_scanner_value(208)
     network.add_wheel_value(1.6)
     network.add_bumper_value(0)
 
-    network.add_scanner_value(83)
+    network.add_scanner_value(229)
     network.add_wheel_value(1.9)
     network.add_bumper_value(0)
 
-    network.add_scanner_value(82)
+    network.add_scanner_value(228)
     network.add_wheel_value(0.4)
     network.add_bumper_value(0)
 
-    network.add_scanner_value(74)
+    network.add_scanner_value(242)
     network.add_wheel_value(2)
     network.add_bumper_value(0)
 
